f0e38b030393006cb3393a5ae38c7789a189b8a1
yichongyang <33671891+yichongyang@users.noreply.github.com>
2024-07-13 23:00:40 -0400

    Initial version

---
+++ src/sort/medium/SortColors.java
@@ -0,0 +1,35 @@
+package sort.medium;
+
+public class SortColors {
+    public static void main(String[] args) {
+        int[] colors = new int[]{2, 0, 2, 1, 1, 0};
+        sortColors(colors);
+        for (int color : colors) {
+            System.out.print(color + " ");
+        }
+    }
+
+    public static void sortColors(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int i = 0;
+        while (i <= high) {
+            if (nums[i] == 0) {
+                swap(nums, i, low);
+                low++;
+                i++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, high);
+                high--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private static void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
